a123d162ae7237c62ca9e770a37a9055104a0c70
Sheep <hank3681@gmail.com>
2022-06-02 01:08:58 +0800

    feat: 完成新增&刪除的動畫效果

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -19,11 +19,36 @@ const changeState = (todo, e) => {
 
 <template>
   <div class="grid gap-y-3.5">
-    <TodoListITem
-      v-for="todo in todos"
-      :key="todo.id"
-      :todo-item="todo"
-      @change-state="changeState(todo, $event)"
-    />
+    <transition-group name="list" appear>
+      <TodoListITem
+        v-for="todo in todos"
+        :key="todo.id"
+        :todo-item="todo"
+        @change-state="changeState(todo, $event)"
+      />
+    </transition-group>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.list {
+  &-enter-from,
+  &-leave-to {
+    opacity: 0;
+    transform: scale(0.6);
+  }
+
+  &-enter-active,
+  &-leave-active {
+    transition: all 0.4s ease;
+  }
+
+  &-leave-active {
+    position: absolute;
+  }
+
+  &-move {
+    transition: all 0.3s ease;
+  }
+}
+</style>
